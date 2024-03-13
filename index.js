b6813b9684c0ed83cb6ed2c1e08918faae903ac6
Alejandro Armas <lxarmas@gmail.com>
2024-03-13 07:06:24 +0000

    list published on front

--- index.js
+++ index.js
@@ -31,14 +31,11 @@ app.set('views', path.join(__dirname, 'views'));
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// GET all books
-app.get('/books', async (req, res) => {
+// Route for the root URL
+app.get('/', async (req, res) => {
   try {
-    // Fetch data from the database
-    const { rows: dbData } = await client.query('SELECT * FROM books');
-
-    // Render the EJS template with data
-    res.render('books', { dbData });
+    const dbData = await fetchDataFromDatabase();
+    res.render('index', { dbData });
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -58,12 +55,29 @@ app.post('/api/books', async (req, res) => {
   }
 });
 
-// Route for the root URL
-app.get('/', (req, res) => {
-  res.render('index.ejs'); // Assuming you want to render the index.ejs file
+// POST a new book with title and author only
+app.post('/books', async (req, res) => {
+    console.log('Received data:', req.body);
+    const { title, author } = req.body;
+  try {
+    await client.query('INSERT INTO books (title, author) VALUES ($1, $2)', [title, author]);
+    res.status(201).send('Book added successfully');
+  } catch (error) {
+    console.error('Error adding book:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+async function fetchDataFromDatabase() {
+  try {
+    const { rows: dbData } = await client.query('SELECT * FROM books');
+    return dbData;
+  } catch (error) {
+    throw new Error('Error fetching data from database: ' + error.message);
+  }
+}
