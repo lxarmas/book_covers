8963079ae37b065c303a723821bb0b0107d248d3
Alejandro Armas <lxarmas@gmail.com>
2024-04-18 22:02:31 +0100

    counter_books

--- public/js/main.js
+++ public/js/main.js
@@ -12,7 +12,7 @@ function initialize(thumbnailUrl) {
 
 
 // Function to delete a book by its ID
-async function deleteBook(bookId) {
+async function deleteBook(bookId,user_id) {
     try {
         const response = await fetch(`/books/${bookId}`, {
             method: 'DELETE',
@@ -21,7 +21,8 @@ async function deleteBook(bookId) {
         if (response.ok) {
             console.log('Book deleted successfully');
             const deletedRow = document.getElementById(`deleteForm_${bookId}`).parentNode;
-            deletedRow.remove();
+          deletedRow.remove();
+           fetchUserBooks(user_id);
         } else {
             console.error('Error deleting book');
         }
@@ -29,16 +30,11 @@ async function deleteBook(bookId) {
         console.error('Error deleting book:', error);
     }
 }
-
-// main.js
-
-// Function to handle logout
 function logout() {
-  // Make an AJAX request to the logout route
-  fetch('/logout', {
-    method: 'POST', // Use POST method
+ fetch('/logout', {
+    method: 'POST', 
     headers: {
-      'Content-Type': 'application/json' // Specify JSON content type
+      'Content-Type': 'application/json' 
     }
   })
   .then(response => {
@@ -55,4 +51,28 @@ function logout() {
     // Handle fetch error
     console.error('Logout failed:', error);
   });
+}
+
+async function fetchUserBooks() {
+    try {
+        const response = await fetch('/user/books');
+        if (response.ok) {
+            const dbData = await response.json();
+            // Call the updateBooksReadHeader function with the fetched book data
+            updateBooksReadHeader(dbData);
+        } else {
+            console.error('Failed to fetch user books:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error fetching user books:', error);
+    }
+}
+
+// Call the fetchUserBooks function to retrieve the user's book data
+fetchUserBooks();
+
+// Function to update the h2 dynamically
+function updateBooksReadHeader(dbData) {
+    const booksReadHeader = document.getElementById('booksReadHeader');
+    booksReadHeader.textContent = `List of Books You Have Read (${dbData.length} books):`;
 }
