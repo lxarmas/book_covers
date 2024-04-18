2c5375aae02145d44391d0d1a4320f1a4fac71b7
Alejandro Armas <lxarmas@gmail.com>
2024-04-18 22:32:25 +0100

    counter_books_two

--- public/js/main.js
+++ public/js/main.js
@@ -12,7 +12,7 @@ function initialize(thumbnailUrl) {
 
 
 // Function to delete a book by its ID
-async function deleteBook(bookId,user_id) {
+async function deleteBook(bookId) {
     try {
         const response = await fetch(`/books/${bookId}`, {
             method: 'DELETE',
@@ -22,7 +22,7 @@ async function deleteBook(bookId,user_id) {
             console.log('Book deleted successfully');
             const deletedRow = document.getElementById(`deleteForm_${bookId}`).parentNode;
           deletedRow.remove();
-           fetchUserBooks(user_id);
+         
         } else {
             console.error('Error deleting book');
         }
@@ -53,26 +53,6 @@ function logout() {
   });
 }
 
-async function fetchUserBooks() {
-    try {
-        const response = await fetch('/user/books');
-        if (response.ok) {
-            const dbData = await response.json();
-            // Call the updateBooksReadHeader function with the fetched book data
-            updateBooksReadHeader(dbData);
-        } else {
-            console.error('Failed to fetch user books:', response.statusText);
-        }
-    } catch (error) {
-        console.error('Error fetching user books:', error);
-    }
-}
-
-// Call the fetchUserBooks function to retrieve the user's book data
-fetchUserBooks();
-
-// Function to update the h2 dynamically
-function updateBooksReadHeader(dbData) {
-    const booksReadHeader = document.getElementById('booksReadHeader');
-    booksReadHeader.textContent = `List of Books You Have Read (${dbData.length} books):`;
+function updateBooksReadHeader( dbData ) {
+  const booksReadHeader = document.getElementById( 'booksReadHeader' );
 }
